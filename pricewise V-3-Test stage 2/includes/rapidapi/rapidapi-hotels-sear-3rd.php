cddbd1b423afc0ce558376f1d8438b1304ff92f4
plimoi <134799550+plimoi@users.noreply.github.com>
2025-02-28 03:59:22 +0200

    Delete pricewise V-3-Test stage 2 directory

--- pricewise V-3-Test stage 2/includes/rapidapi/rapidapi-hotels-sear-3rd.php
+++
@@ -1,9 +0,0 @@
-<?php
-// includes/rapidapi/rapidapi-hotels-sear-3rd.php - Stage 3 API functions stub for future Hotel Prices Search
-
-if ( ! defined( 'ABSPATH' ) ) {
-    exit;
-}
-
-// Placeholder for future implementation of Stage 3 API (Hotel Prices Search)
-// You can add your functions here in the future.
